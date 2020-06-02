f178571776cc81fc03d45d03a4e362070eccbf4d
Hedi Calabrese <calabrese.hedi@gmail.com>
2020-06-01 19:59:32 -0700

    add errors for minus function

--- test/util.test.js
+++ test/util.test.js
@@ -1,4 +1,5 @@
 const { add } = require("../util/util") // double dot to go back from the folder
+const { minus } = require("../util/util")
 
 describe("Add", () => {
     describe("Success", () => {
@@ -14,22 +15,28 @@ describe("Add", () => {
         it("Should throw an error if 2nd argument isn't a number", () => {
             expect(() => (add(2, "2"))).toThrowError() // toBe is only for returned value so it will not work if we dont execute the function, so we need to throw an Error
         });
+        it("Should throw an error if no arguments were reiceived", () => {
+            expect(() => (add())).toThrowError()
+        });
     });
 });
 
 describe("Minus", () => {
     describe("Success", () => {
-        // it("Should return the sum of the 2 passed aguments", () => {
-        //     expect(add(2, 2)).toBe(4); // toBe is talking to the return value from our fonction
-        // });
+        it("Should return the substract of the 2 passed aguments", () => {
+            expect(minus(4, 2)).toBe(2); // toBe is talking to the return value from our fonction
+        });
     })
 
     describe("Failure", () => {
-        // it("Should throw an error if 1st argument isn't a number", () => {
-        //     expect(() => (add("2", 2))).toThrowError() // toBe is only for returned value so it will not work if we dont execute the function, so we need to throw an Error
-        // });
-        // it("Should throw an error if 2nd argument isn't a number", () => {
-        //     expect(() => (add(2, "2"))).toThrowError() // toBe is only for returned value so it will not work if we dont execute the function, so we need to throw an Error
-        // });
+        it("Should throw an error if 1st argument isn't a number", () => {
+            expect(() => (minus("2", 2))).toThrowError() // toBe is only for returned value so it will not work if we dont execute the function, so we need to throw an Error
+        });
+        it("Should throw an error if 2nd argument isn't a number", () => {
+            expect(() => (minus(2, "2"))).toThrowError() // toBe is only for returned value so it will not work if we dont execute the function, so we need to throw an Error
+        });
+        it("Should throw an error if no arguments were reiceived", () => {
+            expect(() => (minus())).toThrowError()
+        });
     });
 });
