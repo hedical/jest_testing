e92071ffb263046581e1bc9ca156471276bf17c0
Hedi Calabrese <calabrese.hedi@gmail.com>
2020-06-01 20:37:20 -0700

    added test for multiply divide and power

--- test/util.test.js
+++ test/util.test.js
@@ -1,5 +1,9 @@
 const { add } = require("../util/util") // double dot to go back from the folder
 const { minus } = require("../util/util")
+const { multiply } = require("../util/util")
+const { divide } = require("../util/util")
+const { power } = require("../util/util")
+
 
 describe("Add", () => {
     describe("Success", () => {
@@ -40,3 +44,64 @@ describe("Minus", () => {
         });
     });
 });
+
+describe("Multiply", () => {
+    describe("Success", () => {
+        it("Should return the multiplication of the 2 passed aguments", () => {
+            expect(multiply(2, 2)).toBe(4); // toBe is talking to the return value from our fonction
+        });
+    })
+
+    describe("Failure", () => {
+        it("Should throw an error if 1st argument isn't a number", () => {
+            expect(() => (multiply("2", 2))).toThrowError() // toBe is only for returned value so it will not work if we dont execute the function, so we need to throw an Error
+        });
+        it("Should throw an error if 2nd argument isn't a number", () => {
+            expect(() => (multiply(2, "2"))).toThrowError() // toBe is only for returned value so it will not work if we dont execute the function, so we need to throw an Error
+        });
+        it("Should throw an error if no arguments were reiceived", () => {
+            expect(() => (multiply())).toThrowError()
+        });
+    });
+});
+
+describe("Divide", () => {
+    describe("Success", () => {
+        it("Should return the division of the 2 passed aguments", () => {
+            expect(divide(2, 2)).toBe(1); // toBe is talking to the return value from our fonction
+        });
+    })
+
+    describe("Failure", () => {
+        it("Should throw an error if 1st argument isn't a number", () => {
+            expect(() => (divide("2", 2))).toThrowError() // toBe is only for returned value so it will not work if we dont execute the function, so we need to throw an Error
+        });
+        it("Should throw an error if 2nd argument isn't a number", () => {
+            expect(() => (divide(2, "2"))).toThrowError() // toBe is only for returned value so it will not work if we dont execute the function, so we need to throw an Error
+        });
+        it("Should throw an error if no arguments were reiceived", () => {
+            expect(() => (divide())).toThrowError()
+        });
+    });
+});
+
+describe("Power", () => {
+    describe("Success", () => {
+        it("Should return the exponent of the first argument using the second one", () => {
+            expect(power(4, 4)).toBe(256); // toBe is talking to the return value from our fonction
+        });
+    })
+
+    describe("Failure", () => {
+        it("Should throw an error if 1st argument isn't a number", () => {
+            expect(() => (power("2", 2))).toThrowError() // toBe is only for returned value so it will not work if we dont execute the function, so we need to throw an Error
+        });
+        it("Should throw an error if 2nd argument isn't a number", () => {
+            expect(() => (power(2, "2"))).toThrowError() // toBe is only for returned value so it will not work if we dont execute the function, so we need to throw an Error
+        });
+        it("Should throw an error if no arguments were reiceived", () => {
+            expect(() => (power())).toThrowError()
+        });
+    });
+});
+
--- util/util.js
+++ util/util.js
@@ -10,5 +10,23 @@ const minus = (num1, num2) => {
     return num1 - num2;
 }
 
+const multiply = (num1, num2) => {
+    if (typeof num1 != "number") throw new Error("1st argument must be a number");
+    if (typeof num2 != "number") throw new Error("2nd argument must be a number");
+    return num1 * num2;
+}
 
-module.exports = { add, minus }; // short way to write module.exports = { add: add };
+const divide = (num1, num2) => {
+    if (typeof num1 != "number") throw new Error("1st argument must be a number");
+    if (typeof num2 != "number") throw new Error("2nd argument must be a number");
+    return num1 / num2;
+}
+
+const power = (num1, num2) => {
+    if (typeof num1 != "number") throw new Error("1st argument must be a number");
+    if (typeof num2 != "number") throw new Error("2nd argument must be a number");
+    return Math.pow(num1, num2);
+}
+
+
+module.exports = { add, minus, multiply, divide, power }; // short way to write module.exports = { add: add };
