c3f440c282f94d3a4dfebf3fa50c8c78c3f0e63c
Hedi Calabrese <calabrese.hedi@gmail.com>
2020-06-01 19:20:07 -0700

    added tests

---
+++ test/util.test.js
@@ -0,0 +1,15 @@
+const { add } = require("../util/util") // double dot to go back from the folder
+
+describe("Add", () => {
+    describe("Success", () => {
+        it("Should return the sum of the 2 passed aguments", () => {
+            expect(add(2, 2)).toBe(4); // toBe is talking to the return value from our fonction
+        });
+    })
+
+    describe("Failure", () => {
+        it("Should throw an error if 1st argument isn't a string", () => {
+            expect(() => (add("2", 2))).toThrowError() // toBe is only for returned value so it will not work if we dont execute the function, so we need to throw an Error
+        });
+    });
+});
--- util/util.js
+++ util/util.js
@@ -1,5 +1,9 @@
-const add = (num1, num2) => num1 + num2;
+const add = (num1, num2) => {
+    if (typeof num1 != "number") throw new Error("1st argument must be a number");
+
+    return num1 + num2;
+}
 
 
 
-module.exports = { add }; // short way to write module.exports = { add: add };
+module.exports = { add }; // short way to write module.exports = { add };
